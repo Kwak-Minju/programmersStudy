ee73776d9d4496457dd511345b2c92e35b6a68ab
Kwak-Minju <67572168+Kwak-Minju@users.noreply.github.com>
2023-09-16 16:26:12 +0900

    [unrated] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -1,21 +1,19 @@
 def solution(code):
     ret=""
     mode = 0
-
     for i in range(len(code)):
-        if mode == 0:
+        if mode ==0:
             if code[i] == '1':
                 mode = 1
+                print("mode change 0 1")
             else:
                 if i %2 ==0:
                     ret += code[i]
         else:
             if code[i] == '1':
                 mode = 0
+                print("mode change 1 0 ")
             else:
                 if i %2 !=0:
                     ret += code[i]
-    if len(ret) == 0:
-        return "EMPTY"
-    else:
-        return ret
+    return ret

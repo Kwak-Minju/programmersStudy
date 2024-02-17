48dd58a9556f969d99f241919117c27fc12bf317
Kwak-Minju <67572168+Kwak-Minju@users.noreply.github.com>
2024-02-17 10:17:44 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = ''
+    cnt = 0
+    
+    for word in s:
+        if word == " ":
+            answer += word
+            cnt = 0
+        elif cnt % 2 == 0:
+            answer += word.upper()
+            cnt += 1
+        else:
+            answer += word.lower()
+            cnt += 1
+        
+            
+    return answer
